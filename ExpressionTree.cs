6911f27f2bd6e5f1bab8ccdb5bf53bab72fde894
Leobardo Mora <darkgem666@gmail.com>
2016-06-23 11:52:22 -0400

    se agrega segundo parametro

--- ExpressionTree.cs
+++ ExpressionTree.cs
@@ -11,21 +11,22 @@ public class ExpressionTree
     private string  infix,postfix = "";
     private StringBuilder constant = new StringBuilder("");
     private ParameterExpression jobcode = Expression.Parameter(typeof(string),"x");
-    private ParameterExpression centro = Expression.Parameter(typeof(string),"centro");
+    private ParameterExpression centro = Expression.Parameter(typeof(string),"y");
     private string operators = "=!&|";
 
-    public Func<string,bool> ContructFunction(string function){
+    public Func<string,string,bool> ContructFunction(string function){
        string postfix = function;
+       Console.WriteLine(postfix);
        string[] lista = postfix.Split(',');
         Stack<Expression> stack = new Stack<Expression>();
       for (int i = 0; i < lista.Length; i++)
       {
           string pos = lista[i];
           if(!operators.Contains(pos)){
-              if (!pos.Equals("x"))
+              if (!pos.Equals("x") || !pos.Equals("y"))
               {
                   string next = lista[i + 1];
-                  if (!next.Equals("x") && !operators.Contains(next))
+                  if (!(next.Equals("x") || pos.Equals("y")) && !operators.Contains(next))
                   {
                       constant.Append(next);
                   }else
@@ -35,11 +36,16 @@ public class ExpressionTree
               }else if (pos.Equals("x"))
               {
                   stack.Push(jobcode);
-                  constant.Clear();
+                  
+              }else if (pos.Equals("y"))
+              {
+                  stack.Push(centro);
               }
+              constant.Clear();
           }else
           {
               constant.Clear();
+             // Console.WriteLine(stack.Count());
               Expression operand1 = stack.Pop();
               Expression operand2 = stack.Pop();
               switch (pos)
@@ -60,10 +66,11 @@ public class ExpressionTree
 
           }
       }
-
+      Console.WriteLine(stack.Count());
       Expression result = stack.Pop();
-      Expression<Func<string,bool>> e = Expression.Lambda<Func<string,bool>>(result,jobcode);
-      Func<string,bool> f = e.Compile();
+      Console.WriteLine(result.ToString());
+      Expression<Func<string,string,bool>> e = Expression.Lambda<Func<string,string,bool>>(result,jobcode,centro);
+      Func<string,string,bool> f = e.Compile();
       return f;
       }
 
