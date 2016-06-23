a5c4d0d713e190e241a5847d992fe48ecb662514
Leobardo Mora <darkgem666@gmail.com>
2016-06-23 16:05:24 -0400

    primera version funcional con 2 parametros

--- ExpressionTree.cs
+++ ExpressionTree.cs
@@ -16,17 +16,17 @@ public class ExpressionTree
 
     public Func<string,string,bool> ContructFunction(string function){
        string postfix = function;
-       Console.WriteLine(postfix);
+      // Console.WriteLine(postfix);
        string[] lista = postfix.Split(',');
         Stack<Expression> stack = new Stack<Expression>();
       for (int i = 0; i < lista.Length; i++)
       {
           string pos = lista[i];
           if(!operators.Contains(pos)){
-              if (!pos.Equals("x") || !pos.Equals("y"))
+              if (!pos.Equals("x") && !pos.Equals("y"))
               {
                   string next = lista[i + 1];
-                  if (!(next.Equals("x") || pos.Equals("y")) && !operators.Contains(next))
+                  if (!(next.Equals("x") && pos.Equals("y")) && !operators.Contains(next))
                   {
                       constant.Append(next);
                   }else
@@ -45,7 +45,7 @@ public class ExpressionTree
           }else
           {
               constant.Clear();
-             // Console.WriteLine(stack.Count());
+              //Console.WriteLine(stack.Peek().ToString());
               Expression operand1 = stack.Pop();
               Expression operand2 = stack.Pop();
               switch (pos)
@@ -66,16 +66,13 @@ public class ExpressionTree
 
           }
       }
-      Console.WriteLine(stack.Count());
+      //Console.WriteLine(stack.Count());
       Expression result = stack.Pop();
-      Console.WriteLine(result.ToString());
+      //Console.WriteLine(result.ToString());
       Expression<Func<string,string,bool>> e = Expression.Lambda<Func<string,string,bool>>(result,jobcode,centro);
       Func<string,string,bool> f = e.Compile();
       return f;
       }
-
-
-
 
 
     }
